5c752e2ba96f9106711d3d3a0e847ea74d860a33
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-19 16:56:11 +0200

    creato e completato file movie.php

---
+++ classes/movie.php
@@ -0,0 +1,34 @@
+<?php
+
+class Movie{
+    public $imgcover;
+    public $title;
+    public $genres;
+    public $duration;
+    public $splat;
+    public $year;
+    public $director;
+
+    public function __construct(string $_imgcover ,string $_title , array $_genre , int $_duration , string $_director){
+        
+        $this -> imgcover = $_imgcover;
+        $this -> title = $_title;
+        $this -> genres = $_genre;
+        $this -> duration = $_duration;
+        $this -> director = $_director;
+        
+    }
+
+    
+
+    // funzione che restituisce titolo e genere
+    public function getInfo(){
+        return "titolo:{$this -> title} e genere:{$this -> genres}";
+    }
+
+    // funzione che restituisce l'indirizzo dell'immagine
+    public function getImage() {
+        return $this-> imgcover;
+    }
+
+}
