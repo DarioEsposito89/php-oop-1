c61946ff0e54682426b1e96b299131ea34add8c8
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-20 09:56:53 +0200

    sistemato spaziatura errata dopo le freccette

--- classes/movie.php
+++ classes/movie.php
@@ -11,11 +11,11 @@ class Movie{
 
     public function __construct(string $_imgcover ,string $_title , array $_genre , int $_duration , string $_director){
         
-        $this -> imgcover = $_imgcover;
-        $this -> title = $_title;
-        $this -> genres = $_genre;
-        $this -> duration = $_duration;
-        $this -> director = $_director;
+        $this->imgcover = $_imgcover;
+        $this->title = $_title;
+        $this->genres = $_genre;
+        $this->duration = $_duration;
+        $this->director = $_director;
         
     }
 
@@ -23,12 +23,12 @@ class Movie{
 
     // funzione che restituisce titolo e genere
     public function getInfo(){
-        return "titolo:{$this -> title} e genere:{$this -> genres}";
+        return "titolo:{$this->title} e genere:{$this->genres}";
     }
 
     // funzione che restituisce l'indirizzo dell'immagine
     public function getImage() {
-        return $this-> imgcover;
+        return $this->imgcover;
     }
 
 }
